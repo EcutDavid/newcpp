96671c23d66389503e5a8656950bfae33cb985ff
David Guan <davidguandev@gmail.com>
2019-01-30 21:14:28 +1100

    tr

--- cli.go
+++ cli.go
@@ -22,10 +22,12 @@ typedef unsigned int u32;
 typedef vector<i32> vi32;
 typedef deque<i32> di32;
 
+#define all(c) (c).begin(), (c).end()
 #define REP(i, a, b) for (auto i = a; i < b; i++)
 #define REPA(i, a, b, acc) for (auto i = a; i < b; i += acc)
 #define PB push_back
 #define PF push_front
+#define TR(c, it) for (auto (it) = (c).begin(); (it) != (c).end(); (it)++)
 
 i32 main() {
   ios::sync_with_stdio(false); // Makes IO faster, remove this line if C style scanf/printf needed.
